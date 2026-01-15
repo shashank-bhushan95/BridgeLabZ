c79728b69626f9fe607d2f32aff5eea7c5125676
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-01-15 14:13:54 +0530

    add new file for js

---
+++ first.js
@@ -0,0 +1,11 @@
+console.log("Node.js Backend Strated");
+
+let name = "Shashank";
+let age = 20;
+const countary = India;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(countary);
+console.log(isStudent);
