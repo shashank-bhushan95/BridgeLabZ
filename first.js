095a7047bda206f0b5ae99e4d16883563f11a5f4
shashank-bhushan95 <shashankbhushan9835@gmail.com>
2026-01-15 14:55:17 +0530

    Commit after Adding Someting

--- first.js
+++ first.js
@@ -9,3 +9,20 @@ console.log(name);
 console.log(age);
 console.log(country);
 console.log(isStudent);
+
+let a = 10;
+let b = 5;
+
+console.log("Add:" , a + b);
+console.log("Sub:" , a - b);
+console.log("Mul:" , a * b);
+console.log("Div:" , a / b);
+
+let loginAge = 18;
+
+if(loginAge >= 18){
+    console.log("Allowed to Login");
+}
+else{
+    console.log("Not Allowed");
+}
